383bddcb8783167450a374bd20c554724996b2ec
vfvicera220 <vfvicera220@gmail.com>
2024-05-09 13:56:01 +0000

    another commit from backup cron

--- backup.sh
+++ backup.sh
@@ -1,3 +1,4 @@
+cd /home/vfvicera220/trnsl8-backup
 /bin/cp -rf /home/vfvicera220/trnsl8-license-server/licenses.db /home/vfvicera220/trnsl8-backup
 git add .
 git commit -m "another commit from backup cron"
