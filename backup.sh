f61dba5662e54d0be8f1311897344667729cf6a3
root <root@instance-20240506-112232.us-west1-b.c.spry-mechanism-307103.internal>
2024-05-12 06:36:05 +0000

    another commit from backup cron

--- backup.sh
+++ backup.sh
@@ -1,4 +1,5 @@
 cd /home/vfvicera220/trnsl8-backup
+mysqldump -u root -p4y2b9tdg wordpress_db | gzip > wpbackup.sql.gz
 /bin/cp -rf /home/vfvicera220/trnsl8-license-server/licenses.db /home/vfvicera220/trnsl8-backup
 git add .
 git commit -m "another commit from backup cron"
